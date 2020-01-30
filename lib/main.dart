88e21bc25571884857357dd9da5dca230f535d70
Annsh Singh <annsh@so.city>
2020-01-30 17:09:02 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:dark_theme/settings.dart';
+import 'package:dark_theme/themes.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    /// Here we are asynchronously passing an instance of SharedPreferences
+    /// to our Settings ChangeNotifier class and that in turn helps us
+    /// determine the basic app settings to be applied whenever the app is
+    /// launched.
+    return FutureBuilder<SharedPreferences>(
+      future: SharedPreferences.getInstance(),
+      builder:
+          (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
+        return ChangeNotifierProvider<Settings>.value(
+          value: Settings(snapshot.data),
+          child: _MyApp(),
+        );
+      },
+    );
+  }
+}
+
+class _MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<Settings>(context).isDarkMode
+          ? setDarkTheme
+          : setLightTheme,
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Theme.of(context).accentColor),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Provider.of<Settings>(context).isDarkMode
+                ? Icons.brightness_high
+                : Icons.brightness_low),
+            onPressed: () {
+              changeTheme(
+                  Provider.of<Settings>(context, listen: false).isDarkMode ? false : true,
+                  context);
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void changeTheme(bool set, BuildContext context) {
+    ///Call setDarkMode method inside our Settings ChangeNotifier class to
+    ///Notify all the listeners of the change.
+    Provider.of<Settings>(context, listen: false).setDarkMode(set);
+  }
+}
---
+++ lib/themes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// Ref: https://github.com/flutter/flutter/blob/master/examples/flutter_gallery/lib/gallery/themes.dart
+
+final setLightTheme = _buildLightTheme();
+final setDarkTheme = _buildDarkTheme();
+
+ThemeData _buildLightTheme() {
+  return ThemeData(
+    primarySwatch: Colors.grey,
+    primaryColor: Colors.white,
+    brightness: Brightness.light,
+    backgroundColor: const Color(0xFFE5E5E5),
+    accentColor: Colors.black,
+    accentIconTheme: IconThemeData(color: Colors.white),
+    dividerColor: Colors.white54,
+  );
+}
+
+ThemeData _buildDarkTheme() {
+  return ThemeData(
+    primarySwatch: Colors.grey,
+    primaryColor: Colors.black,
+    brightness: Brightness.dark,
+    backgroundColor: const Color(0xFF212121),
+    accentColor: Colors.white,
+    accentIconTheme: IconThemeData(color: Colors.black),
+    dividerColor: Colors.black12,
+  );
+}
